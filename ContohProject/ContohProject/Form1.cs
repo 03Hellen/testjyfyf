c6a51041a1edb7a121a084390ddc37bec4211b3f
03Hellen <145331611+03Hellen@users.noreply.github.com>
2023-11-13 15:13:26 +0800

    Add files via upload

---
+++ ContohProject/ContohProject/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace ContohProject
+{
+    public partial class Form1 : Form
+    {
+        private MySqlConnection koneksi;
+        private MySqlDataAdapter adapter;
+        private MySqlCommand perintah;
+
+        private DataSet ds = new DataSet();
+        private string alamat, query;
+        public Form1()
+        {
+            alamat = "server=localhost; database=filkomday; username=root; password=;";
+            koneksi = new MySqlConnection(alamat);
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                koneksi.Open();
+
+                query = string.Format("select * from da where id_data = '{0}'", TxtNIM.Text);
+                perintah = new MySqlCommand(query, koneksi);
+                adapter = new MySqlDataAdapter(perintah);
+                perintah.ExecuteNonQuery();
+                ds.Clear();
+                adapter.Fill(ds);
+                koneksi.Close();
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    foreach (DataRow kolom in ds.Tables[0].Rows)
+                    {
+                        TxtLastName.Text = kolom["lastname"].ToString();
+                        TxtFirstName.Text = kolom["Fist_name"].ToString();
+                       CBProdi.Text = kolom["Jurusan"].ToString();
+
+     
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("NIM tidak ditemukan");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                query = string.Format("insert into da (id_data, lastname, Fist_name, semester, Jurusan) values ('{0}', '{1}', '{2}', '{3}','{4}')", TxtNIM.Text, TxtLastName.Text, TxtFirstName.Text, CBProdi.Text);
+                koneksi.Open();
+                perintah = new MySqlCommand(query, koneksi);
+                adapter = new MySqlDataAdapter(perintah);
+                int res = perintah.ExecuteNonQuery();
+                koneksi.Close();
+                if (res == 1)
+                {
+                    MessageBox.Show("Insert data berhasil");
+                    Form1_Load(null, null);
+                }
+                else
+                {
+                    MessageBox.Show("Insert data gagal");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                koneksi.Open();
+                query = string.Format("update da set lastname = '{0}', Fist_name = '{1}', semester = '{2}', Jurusan = '{3}' where id_data ='{4}'", TxtLastName.Text, TxtFirstName.Text, CBProdi.Text, TxtNIM.Text);
+                perintah = new MySqlCommand(query, koneksi);
+                adapter = new MySqlDataAdapter(perintah);
+                perintah.ExecuteNonQuery();
+                ds.Clear();
+                adapter.Fill(ds);
+                koneksi.Close();
+
+                Form1_Load(null, null);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void CBProdi_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
